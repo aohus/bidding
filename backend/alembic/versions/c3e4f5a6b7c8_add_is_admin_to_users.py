cd31b8728bc1e7ac989ef5df31813d69c8c59e68
aohus <dbtnghk528@gmail.com>
2026-02-20 16:00:11 +0900

    fix: 수정된 down_revision ID 업데이트

--- backend/alembic/versions/c3e4f5a6b7c8_add_is_admin_to_users.py
+++ backend/alembic/versions/c3e4f5a6b7c8_add_is_admin_to_users.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'c3e4f5a6b7c8'
-down_revision = 'b2c3d4e5f6a7'
+down_revision = 'f6a7b8c9d0e1'
 branch_labels = None
 depends_on = None
 
